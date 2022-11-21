01634ede6c8684f9e7c4049b91bb4578ff5769f1
Ricardo Pérez <ricpelo@gmail.com>
2022-11-21 21:20:52 +0100

    Lunes 21 de noviembre

---
+++ ejer5.py
@@ -0,0 +1,2 @@
+edad = int(input('Introduzca su edad: '))
+print('Su edad el año que viene será', edad + 1)
---
+++ suma.py
@@ -0,0 +1,3 @@
+x = int(input('Introduzca el primer número: '))
+y = int(input('Introduzca el segundo número: '))
+print('El resultado es:', x + y)
